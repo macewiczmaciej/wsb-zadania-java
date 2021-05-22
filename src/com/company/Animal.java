b9aa6dcd8e5ce7dbaa502717999f8be11ab8dad3
macewiczmaciej <macewicz.maciej@gmail.com>
2021-05-22 20:36:21 +0200

    Zadanie 1

---
+++ src/com/company/Animal.java
@@ -0,0 +1,37 @@
+package com.company;
+
+public class Animal {
+    public final String species;
+    private Double weight;
+    String name;
+
+    public Animal(String species) {
+        this.species = species;
+        if (this.species == "dog") {
+            this.weight = 5.0;
+        } else if (this.species == "cat") {
+            this.weight = 2.0;
+        } else if (this.species == "mouse") {
+            this.weight = 0.1;
+        }
+    }
+
+    void feed() {
+        if(this.weight>0) {
+            weight += 1;
+            System.out.println("Your pet has been fed");
+        }
+        else{
+            System.out.println("Your pet is dead. You degenerate!");
+        }
+    }
+
+    void takeForAWalk() {
+        if (this.weight > 0) {
+            weight -= 1;
+            System.out.println("Your pet came back from a walk");
+        } else {
+            System.out.println("Your pet is dead. You degenerate!");
+        }
+    }
+}
---
+++ src/com/company/Main.java
@@ -0,0 +1,14 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Animal zenek = new Animal("cat");
+        zenek.takeForAWalk();
+        zenek.feed();
+        zenek.takeForAWalk();
+        zenek.takeForAWalk();
+        zenek.takeForAWalk();
+        zenek.feed();
+    }
+}
