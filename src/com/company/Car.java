31f3cdc051745c22334c99f3e3b173a3de45e96d
macewiczmaciej <macewicz.maciej@gmail.com>
2021-05-22 20:44:09 +0200

    Zadanie 2

---
+++ src/com/company/Car.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class Car {
+    final String producer;
+    final String model;
+    String color;
+    Double engineVolume;
+
+    public Car(String producer, String model) {
+        this.producer = producer;
+        this.model = model;
+    }
+}
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,12 +3,9 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        Animal zenek = new Animal("cat");
-        zenek.takeForAWalk();
-        zenek.feed();
-        zenek.takeForAWalk();
-        zenek.takeForAWalk();
-        zenek.takeForAWalk();
-        zenek.feed();
+
+        Human Hans = new Human();
+        Car passat = new Car("Volkswagen","Passat");
+        Hans.car = passat;
     }
 }
